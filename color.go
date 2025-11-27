82970b347c9c08a4b6e8a6fdaf381d5fa7dac31c
Hajime Hoshi <hajimehoshi@gmail.com>
2025-11-28 00:47:07 +0900

    iro: update comments

--- color.go
+++ color.go
@@ -40,7 +40,7 @@ func ColorFromSRGB(r, g, b, alpha float64) Color {
 	return ColorFromLinearSRGB(r, g, b, alpha)
 }
 
-// ColorFromSRGBColor converts an sRGB color.Color to Color.
+// ColorFromSRGBColor converts an sRGB [color.Color] to Color.
 // As special cases, [color.NRGBA] and [color.NRGBA64] are handled directly without assuming premultiplied alpha.
 // For other [color.Color] values, the RGBA method is used, assuming premultiplied alpha.
 func ColorFromSRGBColor(c color.Color) Color {
@@ -121,7 +121,7 @@ func ColorFromLinearSRGB(r, g, b, alpha float64) Color {
 	}
 }
 
-// ColorFromLinearSRGBColor converts a linear sRGB color.Color to Color.
+// ColorFromLinearSRGBColor converts a linear sRGB [color.Color] to Color.
 func ColorFromLinearSRGBColor(c color.Color) Color {
 	switch v := c.(type) {
 	case color.RGBA:
